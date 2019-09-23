1c751d2c547b38ed03391b55db3e31220a2677d9
Cristian <tudor.cristian.gabriel@gmail.com>
2019-09-23 21:00:23 +0100

    added difficulties

--- Program.cs
+++ Program.cs
@@ -6,10 +6,29 @@ namespace GuessTheNumber
     {
         static void Main(string[] args)
         {
-            int attempts = 20;
             Random random = new Random();
-            int randomNumber = random.Next(100);
-            Console.WriteLine("Input a number between 0 and 100, you have " + attempts + " attempts.");
+            int randomNumber = 0;
+            int attempts = 0;
+            Console.WriteLine("Please select difficulty: 1 for easy, 2 for medium and 3 for hard");
+            int choice = Int32.Parse(Console.ReadLine());
+            switch(choice)
+            {
+                case 1:
+                    attempts = 20;
+                    randomNumber = random.Next(100);
+                    Console.WriteLine("Input a number between 0 and 100, you have " + attempts + " attempts.");
+                    break;
+                case 2:
+                    attempts = 15;
+                    randomNumber = random.Next(500);
+                    Console.WriteLine("Input a number between 0 and 500, you have " + attempts + " attempts.");
+                    break;
+                case 3:
+                    attempts = 7;
+                    randomNumber = random.Next(-1000, 1000);
+                    Console.WriteLine("Input a number between -1000 and 1000, you have " + attempts + " attempts.");
+                    break;
+            }
             int guess = Int32.Parse(Console.ReadLine());
             while(randomNumber != guess)
             {
@@ -23,7 +42,6 @@ namespace GuessTheNumber
                     Console.WriteLine("Oops, wrong number, the number was too low. Try again! You have " + attempts + " attempts left.");
                 else
                     Console.WriteLine("Oops, wrong number, the number was too high. Try again! You have " + attempts + " attempts left.");
-                randomNumber = random.Next(100);
                 Console.WriteLine("Input a number between 0 and 100");
                 guess = Int32.Parse(Console.ReadLine());
             }
