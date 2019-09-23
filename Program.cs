b7eaa0cb5ae7070eb5bac65de4a3c1fae76b79ef
Cristian <tudor.cristian.gabriel@gmail.com>
2019-09-23 20:49:18 +0100

    Finished basic guessing game

--- Program.cs
+++ Program.cs
@@ -6,7 +6,18 @@ namespace GuessTheNumber
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random random = new Random();
+            int randomNumber = random.Next(100);
+            Console.WriteLine("Input a number between 0 and 100");
+            int guess = Int32.Parse(Console.ReadLine());
+            while(randomNumber != guess)
+            {
+                Console.WriteLine("Oops, wrong number, the number was " + randomNumber + ". Try again!");
+                randomNumber = random.Next(100);
+                Console.WriteLine("Input a number between 0 and 100");
+                guess = Int32.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Congrats! You got it");
         }
     }
 }
