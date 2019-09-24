f0692b8f3726a4c7b581c0cce7953eeee89f6910
Cristian <tudor.cristian.gabriel@gmail.com>
2019-09-24 13:49:18 +0100

    Added validation for all inputs

--- Program.cs
+++ Program.cs
@@ -7,15 +7,32 @@ namespace GuessTheNumber
         static void Main(string[] args)
         {
             Console.WriteLine("Choose gamemode:\n1. Player guessing\n2. Computer guessing");
-            int choice = Int32.Parse(Console.ReadLine());
-            if(choice == 1)
+            int choice;
+            bool done = false;
+            while (!done)
             {
-                PlayerGuesses();
+                try
+                {
+                    choice = Int32.Parse(Console.ReadLine());
+                    if (choice == 1 || choice == 2)
+                        done = true;
+                    else
+                        Console.WriteLine("Please input a number between 1 and 2");
+                    if (choice == 1)
+                    {
+                        PlayerGuesses();
+                    }
+                    else if (choice == 2)
+                    {
+                        ComputerGuesses();
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Please input a number between 1 and 2");
+                }
             }
-            else if(choice == 2)
-            {
-                ComputerGuesses();
-            }
+            
         }
         static public void PlayerGuesses()
         {
@@ -23,26 +40,65 @@ namespace GuessTheNumber
             int randomNumber = 0;
             int attempts = 0;
             Console.WriteLine("Please select difficulty: 1 for easy, 2 for medium and 3 for hard");
-            int choice = Int32.Parse(Console.ReadLine());
-            switch (choice)
+            int choice;
+            int minRange = 0, maxRange = 0;
+            bool done = false;
+            while (!done)
             {
-                case 1:
-                    attempts = 20;
-                    randomNumber = random.Next(100);
-                    Console.WriteLine("Input a number between 0 and 100, you have " + attempts + " attempts.");
-                    break;
-                case 2:
-                    attempts = 15;
-                    randomNumber = random.Next(500);
-                    Console.WriteLine("Input a number between 0 and 500, you have " + attempts + " attempts.");
-                    break;
-                case 3:
-                    attempts = 7;
-                    randomNumber = random.Next(-1000, 1000);
-                    Console.WriteLine("Input a number between -1000 and 1000, you have " + attempts + " attempts.");
-                    break;
+                try
+                {
+                    choice = Int32.Parse(Console.ReadLine());
+                    if (choice == 1 || choice == 2 || choice == 3)
+                    {
+                        done = true;
+                        switch (choice)
+                        {
+                            case 1:
+                                attempts = 20;
+                                randomNumber = random.Next(100);
+                                maxRange = 100;
+                                Console.WriteLine("Input a number between 0 and 100, you have " + attempts + " attempts.");
+                                break;
+                            case 2:
+                                attempts = 15;
+                                randomNumber = random.Next(500);
+                                maxRange = 500;
+                                Console.WriteLine("Input a number between 0 and 500, you have " + attempts + " attempts.");
+                                break;
+                            case 3:
+                                attempts = 7;
+                                randomNumber = random.Next(-1000, 1000);
+                                minRange = -1000;
+                                maxRange = 1000;
+                                Console.WriteLine("Input a number between -1000 and 1000, you have " + attempts + " attempts.");
+                                break;
+                        }
+                    }
+                    else
+                        Console.WriteLine("Please input a number between 1 and 3");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Please input a number between 1 and 3");
+                }
             }
-            int guess = Int32.Parse(Console.ReadLine());
+            int guess = -1;
+            done = false;
+            while (!done)
+            {
+                try
+                {
+                    guess = Int32.Parse(Console.ReadLine());
+                    if (guess > minRange && guess < maxRange)
+                        done = true;
+                    else
+                        Console.WriteLine("Please input a number between " + minRange + " and " + maxRange);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Please input a number between " + minRange + " and " + maxRange);
+                }
+            }
             while (randomNumber != guess)
             {
                 attempts--;
@@ -56,7 +112,22 @@ namespace GuessTheNumber
                 else
                     Console.WriteLine("Oops, wrong number, the number was too high. Try again! You have " + attempts + " attempts left.");
                 Console.WriteLine("Input a number between 0 and 100");
-                guess = Int32.Parse(Console.ReadLine());
+                done = false;
+                while (!done)
+                {
+                    try
+                    {
+                        guess = Int32.Parse(Console.ReadLine());
+                        if (guess > minRange && guess < maxRange)
+                            done = true;
+                        else
+                            Console.WriteLine("Please input a number between " + minRange + " and " + maxRange);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Please input a number between " + minRange + " and " + maxRange);
+                    }
+                }
             }
             Console.WriteLine("Congrats! You got it");
         }
@@ -69,7 +140,11 @@ namespace GuessTheNumber
             int minguess = 1;
             int maxguess = 100;
             Console.WriteLine("My guess is " + guess + ". Am I right?\n1. Correct!\n2. Too high!\n3. Too low!");
-            int choice = Int32.Parse(Console.ReadLine());
+            int choice = -1;
+            while(!Int32.TryParse(Console.ReadLine(), out choice) || (choice != 1 && choice != 2 && choice != 3))
+            {
+                Console.WriteLine("Please input a number between 1 and 3");
+            }
             while(choice != 1)
             {
                 attempts--;
@@ -95,7 +170,10 @@ namespace GuessTheNumber
                         maxguess = tempGuess;
                     Console.WriteLine("My guess is " + guess + ". Am I right?\n1. Correct!\n2. Too high!\n3. Too low!");
                 }
-                choice = Int32.Parse(Console.ReadLine());
+                while (!Int32.TryParse(Console.ReadLine(), out choice) || (choice != 1 && choice != 2 && choice != 3))
+                {
+                    Console.WriteLine("Please input a number between 1 and 3");
+                }
             }
             Console.WriteLine("Yay! I won!!");
         }
